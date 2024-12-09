05f59af60118dd5a31cbb5831262de760ec02d06
Linus Rundberg Streuli <linus.rundberg.streuli@gmail.com>
2024-12-09 18:15:21 +0100

    initial commit

---
+++ util.py
@@ -0,0 +1,12 @@
+from itertools import cycle 
+
+directions = {
+    "N": (-1, 0),
+    "NE": (-1, 1),
+    "E": (0, 1),
+    "SE": (1, 1),
+    "S": (1, 0),
+    "SW": (1, -1),
+    "W": (0, -1),
+    "NW": (-1, -1) 
+}
